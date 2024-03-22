d2adea2658125bbe3d9850027260c9dc41ec1b2c
Yukuan Lu <lykuni@ucdavis.edu>
2024-03-22 03:42:57 +0000

    some bugs in human play

--- test_BouncingBall.py
+++ test_BouncingBall.py
@@ -5,17 +5,18 @@ from gym.wrappers import FlattenObservation
 
 from gym.utils.env_checker import check_env
 from gym.utils.play import play, PlayPlot
-# from gym.envs.box2d import CarRacing
+from gym.envs.box2d import CarRacing
 
-def callback(obs_t, obs_tp1, action, rew, terminated, truncated, info):
-    return [action,]
 
-plotter = PlayPlot(callback, 150, ["action"])   
-# env = gym.make('ALE/VideoPinball-v5', render_mode="human")
+env = gym.make('custom_envs/BouncingBall-v0', size=2, ball_diameter_ratio=0.02, render_mode="rgb_array", apply_action=False, log=True)
 
-# env = gym.make('custom_envs/GridWorld-v0', size=7, render_mode="human")
-env = gym.make('custom_envs/BouncingBall-v0', size=2, ball_diameter_ratio=0.02, render_mode="human", apply_action=False, log=True)
-play(env, callback=plotter.callback)
+play(env, keys_to_action = {
+            # Direction vectors represented as [x, y]
+            "w": [0, -1],  # Up
+            "a": [-1, 0],  # Left
+            "s": [0, 1],   # Down
+            "d": [1, 0],   # Right
+        })
 
 # env = gym.make('gym_examples/GridWorld-v0', render_mode="rgb_array")
 
